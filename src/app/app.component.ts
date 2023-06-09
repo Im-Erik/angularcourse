645b6bc9b86bb9b45946cc91cf157f6b9dba64f8
Erik Orrego <erik.orrego.n@gmail.com>
2023-06-09 17:56:26 -0400

    ejercicios tuto youtube

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,74 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angularcourse2023';
+
+  constructor() {
+    //Devuelve en un nuevo array los elementos indicados en la busqueda
+    const testMap = [1, 2, 3, 4, 5, 6, 7, 8, 98].map((item) => item * 3);
+    console.log(testMap);
+
+    //
+    const testForeach = [1, 2, 3, 4, 5, 6, 7, 8, 98].forEach((item) => item);
+    console.log(testForeach);
+
+    //Devuelve el elemento indicado
+    const testFind = [0, 2, 5, 6, 8, 9].find((item) => item === 5);
+    console.log(testFind);
+
+    //Devuelve en un nuevo array los elementos que coincidan con lo indicado
+    const testFilter = [0, 2, 5, 6, 8, 9].filter((item) => item % 2 === 0);
+    console.log(testFilter);
+
+    //Devuelve la posicion del elemente buscado dentro de un array - de no encontrar devolvera -1
+    const testFindIndex = [0, 7, 5, 6, 8, 9].findIndex((item) => item === 7);
+    console.log(testFindIndex);
+
+    //Devuelve la posicion dentro de la cadena del elemento a buscar, funciona con arrays y cadenas - de no encontrar devolvera -1
+    const testIndexOf = [0, 7, 300, 6, 8, 9].indexOf(300);
+    console.log(testIndexOf);
+
+    //Une los elementos devueltos mediante el parametro indicado
+    const testJoin = [0, 7, 300, 6, 8, 9].join('---');
+    console.log(testJoin);
+
+    //Separa la cadena indicada y crea un nuevo array separado por el parametro indicado
+    const testSplit = '1,2,3,4,5,6'.split(',');
+    console.log(testSplit);
+
+    //Crea un nuevo array a partir de la posicion indicada en el primer parametro seguido por la cantidad de elementos indicados como segundo parametro
+    const testSplice = [8, 7, 6, 5, 4, 3, 2, 1, 0].splice(2, 5);
+    console.log(testSplice);
+
+    //Itera y acumula los valores pasando por cada posicion
+    const testReduce = [0, 2, 300, 4, 3, 2].reduce((acc, value) => acc + value);
+    console.log(testReduce);
+
+    //
+    const testEntries = { value: 'ricardo', key: 'RPC' };
+    console.log('aaa', Object.entries(testEntries));
+    console.log('bbb', Object.keys(testEntries));
+    console.log('ccc', Object.values(testEntries));
+
+    //Spread operator concatena mediante "..."
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = [9, 10, 11, ...array1];
+
+    const per1 = { name: 'marco', age: 30 };
+    const per2 = { ciudad: 'santiago', ...per1 };
+
+    console.log('Spread', array2, per2);
+
+    //Desestructuracion Rescata solo la informacion que se necesita, ejemplo con json
+    const per3 = {
+      name: 'miguel',
+      age: 34,
+      ciudad: 'santiago',
+    };
+    const { name:nombre} = per3;
+    console.log('El nombre es =', nombre);
+  } // Fin constructor
 }
